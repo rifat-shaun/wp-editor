ba261f0dbaebed7ea90a27b0ff7ba60c556d538a
rifat-hasan <mdrifathasanshaun@gmail.com>
2025-09-30 01:24:41 +0600

    initialize app

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-success-500 font-bold text-2xl p-4">Welcome to the Lax Editor</div>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "LaxEditor",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "framer-motion"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "framer-motion": "FramerMotion",
+        },
+      },
+    },
+  },
+  test: {
+    projects: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: ".storybook",
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: "playwright",
+            instances: [
+              {
+                browser: "chromium",
+              },
+            ],
+          },
+          setupFiles: [".storybook/vitest.setup.ts"],
+        },
+      },
+    ],
+  },
+});
