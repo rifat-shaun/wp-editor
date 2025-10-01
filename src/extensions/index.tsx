d3bf1b26f096dc78a88ea8fab5317d0a921457d7
rifat-hasan <mdrifathasanshaun@gmail.com>
2025-10-01 14:22:36 +0600

    feat: implement typography extension

--- src/extensions/index.tsx
+++ src/extensions/index.tsx
@@ -1,11 +1,17 @@
 import StarterKit from "@tiptap/starter-kit";
 import CharacterCount from "@tiptap/extension-character-count";
 import { Placeholder } from "@tiptap/extensions";
+import Typography from '@tiptap/extension-typography'
 
 export const EditorExtensions = [
   StarterKit,
   CharacterCount,
   Placeholder.configure({
     placeholder: "Write something …",
+  }),
+  Typography.configure({
+    oneHalf: false,
+    oneQuarter: false,
+    threeQuarters: false,
   }),
 ];
