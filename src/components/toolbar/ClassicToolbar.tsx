7ad016f25983edbdc47d41007de1412a2effbfeb
rifat-hasan <mdrifathasanshaun@gmail.com>
2025-10-08 12:57:57 +0600

    feat: improve design

--- src/components/toolbar/ClassicToolbar.tsx
+++ src/components/toolbar/ClassicToolbar.tsx
@@ -1,8 +1,8 @@
-import { Dropdown, Space, Typography } from "antd";
+import { Dropdown } from "antd";
 import { TOOLBAR_TYPES, TABS, type TTabKey } from "@/constants/Toolbar";
 import { Editor } from "@tiptap/react";
 import { useState } from "react";
-import { ExpandLess, ExpandMore, Menu as MenuIcon } from "@mui/icons-material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 import { useHorizontalScroll } from "@/hooks/useHorizontalScroll";
 import { ScrollableContent } from "./ScrollableContent";
 import { ToolbarDropdown } from "./ToolbarDropdown";
@@ -55,32 +55,27 @@ export const ClassicToolbar = ({
             defaultOpenKeys: [activeTab],
             selectedKeys: [activeTab],
           }}
+          trigger={['click']}
           placement="bottomLeft"
           onOpenChange={(open) => setIsDropdownOpen(open)}
         >
-          <div className="flex items-center gap-1 px-3 py-1 text-sm hover:bg-neutral-50 transition-colors cursor-pointer">
-            <Typography.Link>
-              <Space>
-                <div className="flex items-center justify-center gap-1">
-                  <MenuIcon fontSize="small" />
-                  <div className="w-10">{activeTab}</div>
-                  <div className="transition-all duration-200 ease-in-out">
-                    {isDropdownOpen ? (
-                      <ExpandLess
-                        fontSize="small"
-                        className="transition-transform duration-3000 ease-in-out"
-                      />
-                    ) : (
-                      <ExpandMore
-                        fontSize="small"
-                        className="transition-transform duration-3000 ease-in-out"
-                      />
-                    )}
-                  </div>
-                </div>
-              </Space>
-            </Typography.Link>
-          </div>
+          <Button title="Toolbar Options" className="w-28">
+            <SvgIcon name="menu" strokeWidth={4} />
+            <div className="w-12 text-sm">{activeTab}</div>
+            <div className="transition-all duration-200 ease-in-out">
+              {isDropdownOpen ? (
+                <ExpandLess
+                  fontSize="small"
+                  className="transition-transform duration-3000 ease-in-out"
+                />
+              ) : (
+                <ExpandMore
+                  fontSize="small"
+                  className="transition-transform duration-3000 ease-in-out"
+                />
+              )}
+            </div>
+          </Button>
         </Dropdown>
         <ScrollableContent
           contentScrollerRef={contentScrollerRef}
