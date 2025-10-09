0923cfe81ba9a97b6a3a0f48c1c7d9bbfb486227
Tanvir Ahmed Khan <tkhan@cognitus.com>
2025-10-09 21:12:09 +0600

    refactor: improve BubbleMenus component structure and logic for better readability and functionality

--- src/components/menubar/bubble-menu/index.tsx
+++ src/components/menubar/bubble-menu/index.tsx
@@ -1,37 +1,50 @@
 import { Editor } from "@tiptap/react";
 import { BubbleMenu } from "@tiptap/react/menus";
+import type { EditorState } from "@tiptap/pm/state";
 import { DefaultBubbleMenuContent } from "./DefaultBubbleMenuContent";
 import { LinkMenuContent } from "./LinkMenuContent";
 import { useLinks } from "@/hooks/useLinks";
 import { ImageMenuContent } from "@/extensions/CustomImage/ImageMenuContent";
 
 export const BubbleMenus = ({ editor }: { editor: Editor }) => {
-	const { shouldShowLinkOnBubbleMenu } = useLinks();
+  const { shouldShowLinkOnBubbleMenu } = useLinks();
 
-	const isDefaultBubbleMenuOff = () => {
-		return !editor.isActive('image');
-	};
+  const isDefaultBubbleMenuOff = ({ state }: { state: EditorState }) => {
+	if(editor.isActive("image")) {
+		return false;
+	}
+	const { from, to } = state.selection;
+	if(from !== to) {
+		return true;
+	}
+    return false;
+  };
 
-	return (
-		<>
-			<BubbleMenu editor={editor} shouldShow={isDefaultBubbleMenuOff} options={{ placement: "bottom", shift: { crossAxis: true } }}>
-				<DefaultBubbleMenuContent editor={editor} />
-			</BubbleMenu>
+  return (
+    <>
+      <BubbleMenu
+        editor={editor}
+        shouldShow={isDefaultBubbleMenuOff}
+        options={{ placement: "bottom", shift: { crossAxis: true } }}
+      >
+        <DefaultBubbleMenuContent editor={editor} />
+      </BubbleMenu>
 
-			<BubbleMenu editor={editor} shouldShow={shouldShowLinkOnBubbleMenu} options={{ placement: "bottom", shift: { crossAxis: true } }}>
-				<LinkMenuContent editor={editor} />
-			</BubbleMenu>
-			
-		<BubbleMenu
-			editor={editor}
-			shouldShow={() => editor.isActive('image')}
-			options={{ placement: "top", shift: { crossAxis: true } }}
-		>
-			<ImageMenuContent editor={editor} />
-		</BubbleMenu>
+      <BubbleMenu
+        editor={editor}
+        shouldShow={shouldShowLinkOnBubbleMenu}
+        options={{ placement: "bottom", shift: { crossAxis: true } }}
+      >
+        <LinkMenuContent editor={editor} />
+      </BubbleMenu>
 
-		</>
-	)
-}
-
-
+      <BubbleMenu
+        editor={editor}
+        shouldShow={() => editor.isActive("image")}
+        options={{ placement: "top", shift: { crossAxis: true } }}
+      >
+        <ImageMenuContent editor={editor} />
+      </BubbleMenu>
+    </>
+  );
+};
