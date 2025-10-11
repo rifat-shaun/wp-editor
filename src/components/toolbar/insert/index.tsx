174043385b60125c515248e64e06d9ead3d78473
rifat-hasan <mdrifathasanshaun@gmail.com>
2025-10-11 16:46:09 +0600

    feat: insert options reordering

--- src/components/toolbar/insert/index.tsx
+++ src/components/toolbar/insert/index.tsx
@@ -19,6 +19,65 @@ export const InsertOptions = () => {
   return (
     <>
       <InsertLinkButton activeToolbarType={currentToolbar} />
+
+      <Popover
+        content={
+          <ImageUploadForm
+            onSubmit={() => setIsImageFormOpen(false)}
+            onCancel={() => setIsImageFormOpen(false)}
+          />
+        }
+        trigger="click"
+        placement="bottom"
+        arrow={false}
+        open={isImageFormOpen}
+        onOpenChange={setIsImageFormOpen}
+      >
+        <Button
+          size={isClassicToolbar ? "small" : "medium"}
+          onClick={() => setIsImageFormOpen(true)}
+          title="Insert Image"
+        >
+          {isClassicToolbar ? (
+            <div className="relative flex items-center gap-1">
+              <SvgIcon name="image" strokeWidth={3} />
+              <span className="text-xs">Image</span>
+              <ArrowDropDownOutlined
+                sx={{ fontSize: "14px", color: "inherit" }}
+              />
+            </div>
+          ) : (
+            <div className="relative flex flex-col items-center gap-1">
+              <div className="flex items-center gap-1">
+                <SvgIcon name="image" size={20} strokeWidth={3} />
+                <ArrowDropDownOutlined
+                  sx={{ fontSize: "16px", color: "inherit" }}
+                />
+              </div>
+              <span className="text-xs">Image</span>
+            </div>
+          )}
+        </Button>
+      </Popover>
+
+      <Button
+        size={isClassicToolbar ? "small" : "medium"}
+        onClick={() => handleInsertCodeBlock()}
+        title="Insert Code Block"
+      >
+        {isClassicToolbar ? (
+          <div className="relative flex items-center gap-1">
+            <SvgIcon name="code" strokeWidth={4} />
+            <span className="text-xs">Code Block</span>
+          </div>
+        ) : (
+          <div className="relative flex flex-col items-center gap-1">
+            <SvgIcon name="code" size={20} strokeWidth={4} />
+            <span className="text-xs">Code Block</span>
+          </div>
+        )}
+      </Button>
+
       <Popover
         content={
           <DividerDropdownContent
@@ -58,62 +117,6 @@ export const InsertOptions = () => {
         </Button>
       </Popover>
 
-      <Button
-        size={isClassicToolbar ? "small" : "medium"}
-        onClick={() => handleInsertCodeBlock()}
-        title="Insert Code Block"
-      >
-        {isClassicToolbar ? (
-          <div className="relative flex items-center gap-1">
-            <SvgIcon name="code" strokeWidth={4} />
-            <span className="text-xs">Code Block</span>
-          </div>
-        ) : (
-          <div className="relative flex flex-col items-center gap-1">
-            <SvgIcon name="code" size={20} strokeWidth={4} />
-            <span className="text-xs">Code Block</span>
-          </div>
-        )}
-      </Button>
-      <Popover
-        content={
-          <ImageUploadForm
-            onSubmit={() => setIsImageFormOpen(false)}
-            onCancel={() => setIsImageFormOpen(false)}
-          />
-        }
-        trigger="click"
-        placement="bottom"
-        arrow={false}
-        open={isImageFormOpen}
-        onOpenChange={setIsImageFormOpen}
-      >
-        <Button
-          size={isClassicToolbar ? "small" : "medium"}
-          onClick={() => setIsImageFormOpen(true)}
-          title="Insert Image"
-        >
-          {isClassicToolbar ? (
-            <div className="relative flex items-center gap-1">
-              <SvgIcon name="image" strokeWidth={3} />
-              <span className="text-xs">Image</span>
-              <ArrowDropDownOutlined
-                sx={{ fontSize: "14px", color: "inherit" }}
-              />
-            </div>
-          ) : (
-            <div className="relative flex flex-col items-center gap-1">
-              <div className="flex items-center gap-1">
-                <SvgIcon name="image" size={20} strokeWidth={3} />
-                <ArrowDropDownOutlined
-                  sx={{ fontSize: "14px", color: "inherit" }}
-                />
-              </div>
-              <span className="text-xs">Image</span>
-            </div>
-          )}
-        </Button>
-      </Popover>
       <Button
         size={isClassicToolbar ? "small" : "medium"}
         onClick={() => handleInsertLineBreak()}
